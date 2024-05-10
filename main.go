2c9212480d3a1ba292f0545003bf91617d419b0f
hongping1224 <hongping1224@gmail.com>
2024-05-10 11:31:59 +0800

    support output relativepath

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 	"github.com/hongping1224/lidario"
 )
 
-func generatePointCloudList(inputRoot string) {
+func generatePointCloudList(inputRoot string, useRelative bool) {
 	lasPaths := []string{inputRoot}
-	lasPaths = findFile(inputRoot, ".las")
+	lasPaths = findFile(inputRoot, ".las", useRelative)
 	pointCloudListPath := filepath.Join(inputRoot, "point_cloud_map_list.csv")
 	f, err := os.Create(pointCloudListPath)
 	if err != nil {
@@ -24,29 +24,33 @@ func generatePointCloudList(inputRoot string) {
 	w := bufio.NewWriter(f)
 	defer w.Flush()
 	for _, lasPath := range lasPaths {
-		fmt.Println(lasPath)
 		las, err := lidario.NewLasFile(lasPath, "rh")
+		if useRelative {
+			lasPath = lasPath[strings.Index(lasPath, "point_cloud_map"):]
+		}
+		fmt.Println(lasPath)
 		if err != nil {
 			log.Printf("%s read fail. Err : %v", las, err)
 			continue
 		}
-		defer las.Close()
 		xmin := las.Header.MinX
 		ymin := las.Header.MinY
 		zmin := las.Header.MinZ
 		xmax := las.Header.MaxX
 		ymax := las.Header.MaxY
 		zmax := las.Header.MaxZ
+
 		if _, err := w.WriteString(fmt.Sprintf("%s,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f\n", lasPath, xmin, ymin, zmin, xmax, ymax, zmax)); err != nil {
 			log.Printf("fail to write csv file %v", err)
 			return
 		}
+		las.Close()
 	}
 }
 
-func generateVectorMapList(inputRoot string) {
+func generateVectorMapList(inputRoot string, useRelative bool) {
 	shpPaths := []string{inputRoot}
-	shpPaths = findFile(inputRoot, ".shp")
+	shpPaths = findFile(inputRoot, ".shp", useRelative)
 	shpListPath := filepath.Join(inputRoot, "vector_map_list.csv")
 	f, err := os.Create(shpListPath)
 	if err != nil {
@@ -56,6 +60,9 @@ func generateVectorMapList(inputRoot string) {
 	w := bufio.NewWriter(f)
 	defer w.Flush()
 	for _, shpPath := range shpPaths {
+		if useRelative {
+			shpPath = shpPath[strings.Index(shpPath, "vector_map"):]
+		}
 		fmt.Println(shpPath)
 		if _, err := w.WriteString(fmt.Sprintf("%s,0\n", shpPath)); err != nil {
 			log.Printf("fail to write csv file %v", err)
@@ -67,11 +74,13 @@ func generateVectorMapList(inputRoot string) {
 
 func main() {
 	dir := flag.String("dir", "./hd_maps", "input Folder")
+	relativePath := flag.Bool("rel", false, "use relative path")
 	flag.Parse()
-	inputRoot, err := filepath.Abs(*dir)
-	if err != nil {
-		log.Fatal("Fail to read Abs input Folder path")
-		return
+	inputRoot := *dir
+	fmt.Println(*dir)
+	fmt.Println(*relativePath)
+	if !*relativePath {
+		inputRoot, _ = filepath.Abs(*dir)
 	}
 	fileinfo, err := os.Stat(inputRoot)
 	if os.IsNotExist(err) {
@@ -81,21 +90,23 @@ func main() {
 		log.Fatal("Fail to read input Folder path")
 		return
 	}
-	generatePointCloudList(inputRoot)
-	generateVectorMapList(inputRoot)
+	generatePointCloudList(inputRoot, *relativePath)
+	generateVectorMapList(inputRoot, *relativePath)
 }
 
-func findFile(root string, match string) (file []string) {
+func findFile(root string, match string, useRelative bool) (file []string) {
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
-		path, err = filepath.Abs(path)
-		if err != nil {
-			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
-			return err
+		if useRelative == false {
+			path, err = filepath.Abs(path)
+			if err != nil {
+				fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+				return err
+			}
 		}
 		if strings.HasSuffix(info.Name(), match) {
 			file = append(file, path)
